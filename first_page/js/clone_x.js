c50e5d0e2c72198feba1f8bbdfd9e4a2fe898cd7
Shugaroon <rnballard@naver.com>
2024-04-11 23:06:10 +0900

    before final changed

---
+++ first_page/js/clone_x.js
@@ -0,0 +1,9 @@
+
+        const btn2 = document.querySelector("btn2");
+    
+        btn2.addEventListener('click', );
+
+        function signUp(){
+           
+            var win = window.open("first_page/signUp_x.html","signUpPop", "width=500", "height=600");
+        }
